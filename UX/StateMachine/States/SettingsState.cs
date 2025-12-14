d56e32daa9b8d7365cefc6a4fea3ed9547f2db72
Jalen Edusei <147884926+jke48222@users.noreply.github.com>
2025-12-14 15:31:51 -0500

    Create SettingsState.cs

---
+++ UX/StateMachine/States/SettingsState.cs
@@ -0,0 +1,9 @@
+namespace XrShared.UX.StateMachine.States
+{
+    public sealed class SettingsState : IAppState
+    {
+        public void Enter() { }
+        public void Exit() { }
+        public void Tick(float dt) { }
+    }
+}
