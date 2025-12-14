b9f350366016d6f8a2d2027f763bdec254fe8e34
Jalen Edusei <147884926+jke48222@users.noreply.github.com>
2025-12-14 15:38:01 -0500

    Create AppEntry_MRVisualSymphony.cs

---
+++ Apps/MRVisualSymphony/Assets/_App/Scripts/AppEntry_MRVisualSymphony.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace MRVisualSymphony
+{
+    public sealed class AppEntry_MRVisualSymphony : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("[MRVisualSymphony] AppEntry started. Add app-specific wiring here later.");
+        }
+    }
+}
