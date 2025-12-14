31c7412857ed11486f829140e3da7b2cd1a36c1f
Jalen Edusei <147884926+jke48222@users.noreply.github.com>
2025-12-14 15:38:39 -0500

    Create AppEntry_ElementalSandbox.cs

---
+++ Apps/ElementalBendingSandbox/Assets/_App/Scripts/AppEntry_ElementalSandbox.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace ElementalBendingSandbox
+{
+    public sealed class AppEntry_ElementalSandbox : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("[ElementalSandbox] AppEntry started. Add app-specific wiring here later.");
+        }
+    }
+}
